cbd281bb12013b81ee79df2585143d1b694a61da
Ayushi Ambekar <ayushiambekar@Ayushis-MacBook-Pro.local>
2020-01-18 21:39:08 -0800

    Wave_Page still not working

---
+++ app/src/main/java/com/example/hackdavis_waterproject/Water_Page.java
@@ -0,0 +1,14 @@
+package com.example.hackdavis_waterproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Water_Page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_water__page);
+    }
+}
