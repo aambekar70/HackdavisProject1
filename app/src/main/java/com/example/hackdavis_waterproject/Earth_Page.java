cf6d05ec64de21a3976fb0fd0c7c2a65ae1c71a8
Ayushi Ambekar <ayushi.ambekar123@gmail.com>
2020-01-19 15:14:15 -0800

    fourth commit

--- app/src/main/java/com/example/hackdavis_waterproject/Earth_Page.java
+++ app/src/main/java/com/example/hackdavis_waterproject/Earth_Page.java
@@ -1,19 +1,28 @@
 package com.example.hackdavis_waterproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.ListViewAutoScrollHelper;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.TextView;
+import java.util.ArrayList;
 
 public class Earth_Page extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_earth__page);
 
-        TextView resource = (TextView) findViewById(R.id.resource);
+        Intent intent_earth = getIntent();
 
-        resource.setText("https://www.thredup.com/fashionfootprint/");
+
+
+
     }
 }
